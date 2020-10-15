b96c60f886e6e16dfc3ecac7e2d54662a353b30d
thangnvh <thangnvh@vng.com.vn>
2020-10-15 23:31:09 +0700

    Initial Commit

---
+++ zalo-swiftui-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  zalo-swiftui-demo
+//
+//  Created by Thang Nguyen on 10/15/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ zalo-swiftui-demoTests/zalo_swiftui_demoTests.swift
@@ -0,0 +1,33 @@
+//
+//  zalo_swiftui_demoTests.swift
+//  zalo-swiftui-demoTests
+//
+//  Created by Thang Nguyen on 10/15/20.
+//
+
+import XCTest
+@testable import zalo_swiftui_demo
+
+class zalo_swiftui_demoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
